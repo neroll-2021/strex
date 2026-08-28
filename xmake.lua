74eca96347c4564135aa6be0d6b05ba798c01232
Neroll <1343809614@qq.com>
2026-08-28 23:28:39 +0800

    chore: bump version to 0.1.1

--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,7 @@
 set_xmakever('2.9.8')
 
 set_project("strex")
-set_version("0.1.0", { build = "%Y%m%d%H%M" })
+set_version("0.1.1", { build = "%Y%m%d%H%M" })
 
 set_allowedplats("windows", "linux")
 set_allowedmodes("debug", "release")
